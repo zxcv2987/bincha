4002f8a7a08ad7d4a4d25276f87e2f1fc3e177dd
태양 <zxcv2987@naver.com>
2025-03-07 20:53:58 +0900

    feat: layout 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import "@/styles/globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,7 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <main className="mx-auto flex max-w-4xl px-4 py-24">{children}</main>
       </body>
     </html>
   );
