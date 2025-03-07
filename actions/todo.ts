b40738c1177819dd04cc41b9af0763a22528dab8
태양 <zxcv2987@naver.com>
2025-03-07 20:50:42 +0900

    feat: actions 추가 - data 클라이언트단 검증 및 api 에러 핸들링

---
+++ actions/todo.ts
@@ -0,0 +1,33 @@
+"use server";
+
+import { createTodo, deleteTodo } from "@/apis/todo";
+
+export async function todoFormAction(state: any, formData: FormData) {
+  const title = formData.get("title");
+  if (title === null || title === "")
+    return { ok: false, error: { title: "할 일을 입력해 주세요." } };
+  const text = formData.get("text");
+  if (text === null || text === "")
+    return { ok: false, error: { text: "내용을 입력해 주세요." } };
+  const categoryId = formData.get("category");
+  if (categoryId === null)
+    return { ok: false, error: { categoryId: "카테고리를 선택해 주세요." } };
+
+  const res = await createTodo(
+    title as string,
+    text as string,
+    categoryId as unknown as number,
+  );
+
+  return { ok: true };
+}
+
+export async function deleteTodoAction(state: any, categoryId: number) {
+  try {
+    await deleteTodo(categoryId);
+    return { ok: true };
+  } catch (error) {
+    console.error("할 일 삭제 중 오류 발생:", error);
+    return { ok: false, error: "삭제 실패" };
+  }
+}
