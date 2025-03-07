7e13dbae4e0e12f8833035ef59b428fdf4523d4e
태양 <zxcv2987@naver.com>
2025-03-07 20:58:43 +0900

    feat: Page 설정

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,20 @@
-export default function Home() {
-  return <div>Home</div>;
+import { getCategory } from "@/apis/category";
+import { getTodos } from "@/apis/todo";
+import TodoList from "@/components/todoList/TodoList";
+import { CategoryType } from "@/types/category";
+import { TodoType } from "@/types/todos";
+import Link from "next/link";
+
+export default async function Home() {
+  const categories: CategoryType[] = await getCategory();
+  const todos: TodoType[] = await getTodos();
+  console.log(todos);
+  return (
+    <div className="flex w-full flex-col gap-4">
+      <Link href="/" className="py-4 text-4xl font-semibold">
+        내가 해야 할 일
+      </Link>
+      <TodoList todos={todos} categories={categories} />
+    </div>
+  );
 }
