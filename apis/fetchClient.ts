4a2401cf61cdfcf4c5cc53290237297706faf911
태양 <zxcv2987@naver.com>
2025-03-08 19:35:06 +0900

    fix:  baseURL 의존 변경

--- apis/fetchClient.ts
+++ apis/fetchClient.ts
@@ -1,11 +1,10 @@
 "use server";
 
-import { headers } from "next/headers";
-
 export default async function FetchClient(url: string, init: RequestInit) {
-  const host = (await headers()).get("host");
-  const protocol = process.env.NODE_ENV === "development" ? "http" : "https";
-  const baseURL = `${protocol}://${host}${url}`;
+  const baseURL =
+    process.env.NODE_ENV === "development"
+      ? `http://localhost:3000/${url}`
+      : `${process.env.NEXT_PUBLIC_BASE_URL}/${url}`;
 
   return (await fetch(baseURL, init)).json();
 }
