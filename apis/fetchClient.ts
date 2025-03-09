7720ecae8e18d53a523a98880965c4a8cbe7c864
태양 <zxcv2987@naver.com>
2025-03-09 20:58:15 +0900

    fix: 환경변수 에러 해결

--- apis/fetchClient.ts
+++ apis/fetchClient.ts
@@ -4,11 +4,10 @@ import { headers } from "next/headers";
 
 export default async function FetchClient(url: string, init: RequestInit) {
   const host = (await headers()).get("host");
-  const protocol =
-    (await headers()).get("x-forwarded-proto") === "https" ? "https" : "http";
+  const protocol = (await headers()).get("x-forwarded-proto");
 
   const baseURL =
-    process.env.NODE_ENV === "development"
+    process.env.NODE_ENV && process.env.NODE_ENV === "development"
       ? `http://localhost:3000/${url}`
       : `${protocol}://${host}/${url}`;
 
