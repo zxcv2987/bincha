cc38cf309395b40adc1b8101bfb5c33a366ca57d
태양 <zxcv2987@naver.com>
2025-04-17 23:01:47 +0900

    chore: prisma, Next.js 최적화

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["localhost:3000", "your-production-domain.com"],
+    },
+  },
+  poweredByHeader: false,
+  compress: true,
+  images: {
+    formats: ["image/avif", "image/webp"],
+  },
 };
 
 export default nextConfig;
--- prisma/prismaClient.ts
+++ prisma/prismaClient.ts
@@ -4,6 +4,19 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
-export const prisma = global.prisma || new PrismaClient();
+export const prisma =
+  global.prisma ||
+  new PrismaClient({
+    log:
+      process.env.NODE_ENV === "development"
+        ? ["query", "error", "warn"]
+        : ["error"],
+    // 연결 시간 초과를 늘림
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
+  });
 
 if (process.env.NODE_ENV !== "production") global.prisma = prisma;
