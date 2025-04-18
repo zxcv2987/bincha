ef6fcba682f8d28c04fdecb737858f97bf755feb
태양 <zxcv2987@naver.com>
2025-04-19 00:35:35 +0900

    fix: token 검증 로직 수정 ( middleware.ts) - fetchClient에 항상 인증 헤더 부착

--- apis/fetchClient.ts
+++ apis/fetchClient.ts
@@ -1,18 +1,28 @@
 "use server";
 
+import { cookies } from "next/headers";
+
 export default async function FetchClient(url: string, init: RequestInit) {
-  try {
-    const baseURL =
-      process.env.NODE_ENV && process.env.NODE_ENV === "development"
-        ? `http://localhost:3000/${url}`
-        : process.env.NEXT_PUBLIC_BASE_URL + url;
+  const baseURL =
+    process.env.NODE_ENV && process.env.NODE_ENV === "development"
+      ? `http://localhost:3000${url}`
+      : `${process.env.NEXT_PUBLIC_BASE_URL}${url}`;
 
-    const response = await fetch(baseURL, init);
-    const data = await response.json();
+  const accessToken = (await cookies()).get("access_token")?.value;
 
-    return data;
-  } catch (error) {
-    console.error(`FetchClient 오류 (${url}):`, error);
-    throw new Error(`API 요청 실패`);
+  init = {
+    ...init,
+    headers: {
+      ...init.headers,
+      Authorization: accessToken ? `Bearer ${accessToken}` : "",
+    },
+  };
+  const response = await fetch(baseURL, init);
+
+  // 응답 상태 확인
+  if (!response.ok) {
+    throw new Error(`API 요청 실패: ${response.status} ${response.statusText}`);
   }
+
+  return await response.json();
 }
