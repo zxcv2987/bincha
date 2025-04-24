266ef235d1cdaeb34bacb024095433b479d28385
태양 <zxcv2987@naver.com>
2025-04-24 23:52:02 +0900

    feat: url 이동 가능하도록 정규식 추가

--- components/ui/Content.tsx
+++ components/ui/Content.tsx
@@ -1,9 +1,32 @@
+import Link from "next/link";
+
 export default function Content({ content }: { content: string }) {
+  const linkifyText = (text: string) => {
+    const urlRegex = /(https?:\/\/[^\s]+)/g;
+
+    return text.split(urlRegex).map((part, i) => {
+      if (part.match(urlRegex)) {
+        return (
+          <Link
+            key={i}
+            href={part}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-zinc-400 underline underline-offset-2"
+          >
+            {part}
+          </Link>
+        );
+      }
+      return part;
+    });
+  };
+
   return (
     <>
       {content.split("\n").map((item, index) => (
-        <p className="flex flex-wrap break-words" key={index}>
-          {item}
+        <p className="flex w-full flex-wrap break-words" key={index}>
+          {linkifyText(item)}
         </p>
       ))}
     </>
